de4256557571725f49756e454b14e0cdeea90b18
Nic Ryan <21nryan@cr.k12.ia.us>
2019-05-11 18:54:49 -0500

    Fixed waffle counter calculation issue

--- app.js
+++ app.js
@@ -1,20 +1,15 @@
 var date = new Date();
+var waffleDate = new Date('August 24, 1860 00:00:00');
+
 var year = date.getFullYear();
 
 var footerYear = document.getElementById('currentYear');
 footerYear.innerHTML = year;
 
-var milliseconds = date.getMilliseconds();
-var seconds = date.getSeconds();
-var minutes = date.getMinutes();
-var hours = date.getHours();
-var days = date.getDay();
-var months = date.getMonth();
+var millisecsElapsed = date - waffleDate;
+var daysElapsed = millisecsElapsed/86400000;
 
-var secsElapsedSinceInventionOfWaffle = (milliseconds/60) + seconds + minutes*60 + hours*3600
-+ days*60*60*24 + months*60*60*24*30 + year*60*60*24*365;
-
-var wafflesEaten = secsElapsedSinceInventionOfWaffle * 4167;
+var wafflesEaten = daysElapsed*360000000;
 var stringWafflesEaten = wafflesEaten.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 
 var waffleNumber = document.getElementById('waffleNumber');
